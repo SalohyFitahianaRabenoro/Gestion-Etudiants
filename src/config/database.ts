dcc6e3fdb4aacd8ae3486879db154bf87846c0a4
Salohy RABENORO <salohyftahiana@gmail.com>
2026-08-11 13:15:59 +0300

    Modification de code dans database dasn le dossier cofig

---
+++ src/config/database.ts
@@ -0,0 +1,31 @@
+import { Pool } from "../../node_modules/@types/pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+export const testConnection = async (): Promise<void> => {
+
+    try {
+
+        await pool.query("SELECT NOW()");
+
+        console.log("Connexion à PostgreSQL réussie !");
+
+    } catch (error) {
+
+        console.error(
+            "Erreur de connexion à PostgreSQL :",
+            error
+        );
+    }
+};
+
+export default pool;
